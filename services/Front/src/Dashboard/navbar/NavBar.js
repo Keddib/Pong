6b0e0bdea6cfcf1abb0851035f4a53c9ce5789f5
keddib <keddib@student.1337.ma>
2022-06-13 11:50:33 +0100

    add the new logo to navbar

--- services/Front/src/Dashboard/navbar/NavBar.js
+++ services/Front/src/Dashboard/navbar/NavBar.js
@@ -1,10 +1,10 @@
-import logo from "../../../public/assets/images/LOGO2.png";
 import HomeIcon from "../../../public/assets/icons/house.svg";
 import BoardIcon from "../../../public/assets/icons/chart-simple.svg";
 import ChatIcon from "../../../public/assets/icons/comment-dots.svg";
 import FriendsIcon from "../../../public/assets/icons/user-group.svg";
 import GroupIcon from "../../../public/assets/icons/users.svg";
 import ProfileIcon from "../../../public/assets/icons/user.svg";
+import Logo from "../../Components/Logo";
 
 import IconWrap from "./components/Icon";
 
@@ -12,45 +12,18 @@ export default function NavBar() {
   return (
     <div className="navbar gradientSpaceCadet shadow-sm rounded-r-2xl">
       <div className=" h-full py-10 flex flex-col">
-        {/* change to link later */}
-        <div className="w-full h-[48px] mb-16 cursor-pointer">
-          <img
-            className="w-[46px] mx-auto"
-            src={logo}
-            alt="pong logo"
-          />
+        <div className="mb-16 cursor-pointer flex justify-center ">
+          <Logo className="group-hover:animate-bounce" />
         </div>
-        <IconWrap
-          icon={
-            <HomeIcon className="w-8 homeIcon " />
-          }
-        />
-        <IconWrap
-          icon={
-            <BoardIcon className="w-[30px] homeIcon " />
-          }
-        />
-        <IconWrap
-          icon={
-            <ChatIcon className="w-8 homeIcon " />
-          }
-        />
-        <IconWrap
-          icon={
-            <FriendsIcon className="w-8 homeIcon " />
-          }
-        />
-        <IconWrap
-          icon={
-            <GroupIcon className="w-[35px] homeIcon " />
-          }
-        />
+        <IconWrap icon={<HomeIcon className="w-8 homeIcon " />} />
+        <IconWrap icon={<BoardIcon className="w-[30px] homeIcon " />} />
+        <IconWrap icon={<ChatIcon className="w-8 homeIcon " />} />
+        <IconWrap icon={<FriendsIcon className="w-8 homeIcon " />} />
+        <IconWrap icon={<GroupIcon className="w-[35px] homeIcon " />} />
         <div className="grow"></div>
         <IconWrap
           className="mb-auto"
-          icon={
-            <ProfileIcon className="w-[28px] homeIcon " />
-          }
+          icon={<ProfileIcon className="w-[28px] homeIcon " />}
         />
       </div>
     </div>
