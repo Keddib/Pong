270c6abbec2611cda15af20eb223ac7cba3778a4
Mohamed Alaoui <malaoui@e1r10p7.1337.ma>
2022-06-17 07:40:34 +0100

    adding users to database after extracting public data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,10 @@
 import { Controller, Get, Query, Post, Body, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UseGuards } from '@nestjs/common';
-import { CreateUserDto } from './users/dto/create-user.dto';
-import { User } from './users/entities/user.entity';
 import { AuthGuard, PassportStrategy } from '@nestjs/passport';
-import {Strategy} from 'passport-42'
+import { Strategy } from 'passport-42'
 import { ConfigService } from '@nestjs/config';
 
-const Profile = {
-  'id': function (obj) { return obj.id;},
-  'username': 'login',
-  'displayName': 'displayname',
-  'name.familyName': 'last_name',
-  'name.givenName': 'first_name',
-  'profileUrl': 'url',
-  'emails.0.value': 'email',
-  'phoneNumbers.0.value': 'phone',
-  'photos.0.value': 'image_url'
-}
 
 
 @Controller()
@@ -50,30 +37,9 @@ export class fortyTwoAuth extends PassportStrategy(Strategy, "42") {
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-
-  // @Post('login')
-  // @Get()
-  // authUser(@Body() b){
-  
-
-  //   console.log(b.user);
-    // const user = new CreateUserDto;
-
-    // user.avatar = b.avatar;
-    // user.displayedName = b.displayName;
-    // user.login = b.login;
-    // user.email = b.email;
-    // user.password = b.password;
-    // console.log(b);
-    // console.log(user);
-    // return this.appService.authUser(user);
-  // }
-
   @Get()
   getHello(@Query() code ): Promise<any> {
 
-    // console.log(req);
-    // console.log(res); 
     return this.appService.getHello(code);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -19,53 +19,56 @@ export class AppService {
 
   async getHello(@Query() code): Promise<any> {
 
+    try {
+      console.log(code);
+      const authToken = await axios({
+      
+        url: "https://api.intra.42.fr/oauth/token",
+        method: "POST",
+        data: {
+          grant_type: "authorization_code",
+          client_id: this.apiConf.get<string>('clientID'),
+          client_secret: this.apiConf.get<string>('clientSecret'),
+          code: code['code'],
+          redirect_uri: this.apiConf.get<string>('callbackURL'),
+        }
 
-    const authToken = await axios({
+      });
     
-      url: "https://api.intra.42.fr/oauth/token",
-      method: "POST",
-      data: {
-        grant_type: "authorization_code",
-        client_id: this.apiConf.get<string>('clientID'),
-        client_secret: this.apiConf.get<string>('clientSecret'),
-        code: code['code'],
-        redirect_uri: this.apiConf.get<string>('callbackURL'),
+      console.log('extracting authToken');
+      const token =  authToken.data["access_token"];
+      // console.log(token);
+      console.log(token);
+      const userData = await axios({
+        url: "https://api.intra.42.fr/v2/me",
+        method: "GET",
+        headers: {
+          "Authorization": "Bearer " + token
+        }
+      })
+      // const test = await this.userRepository.findOneBy({ login: userData.data.login });
+      // console.log(test);
+      // To check later for all user details and update
+      if (await this.userRepository.findOneBy({ login: userData.data.login })) {
+        return "User already in Database";
       }
 
-    });
+      console.log(userData.data.email);
+      const user = new CreateUserDto;
 
-    const token =  authToken.data["access_token"];
-    // console.log(token);
-
-    const userData = await axios({
-      url: "https://api.intra.42.fr/v2/me",
-      method: "GET",
-      headers: {
-        "Authorization": "Bearer " + token
-      }
-    })
-    // To check later for all user details and update
-    if (this.userRepository.find( { where: [{login: userData.data.login} ] })) {
-      return "User already in Database";
+      user.email = userData.data.email;
+      user.displayedName = userData.data.login;
+      user.avatar = userData.data.image_url;
+      user.login = userData.data.login;
+      user.password = 'defaultpassword';
+      this.userRepository.create(user);
+      this.userRepository.save(user);
     }
+    catch (error) {
 
-    console.log(userData.data.email);
-    const user = new CreateUserDto;
-
-    user.email = userData.data.email;
-    user.displayedName = userData.data.login;
-    user.avatar = userData.data.image_url;
-    user.login = userData.data.login;
-    user.password = 'defaultpassword';
-    this.userRepository.create(user);
-    return this.userRepository.save(user);
+      return error;
+    }
+    return "Added New User To database check /users";
   }
 
-  
-  async authUser(user: CreateUserDto): Promise<User> {
-
-    this.userRepository.create(user);
-    console.log('created user');
-    return this.userRepository.save(user);
-  }
 }
