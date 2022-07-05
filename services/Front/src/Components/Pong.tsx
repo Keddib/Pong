f1d059ac1763e3664f36c91ef0b51fe751638200
Yahya Oukharta <youkhart@e1r6p1.1337.ma>
2022-07-05 18:31:55 +0100

    update on game canvas responsiveness, useRef instead of local variables

--- services/Front/src/Components/Pong.tsx
+++ services/Front/src/Components/Pong.tsx
@@ -1,10 +1,24 @@
 import Sketch from "react-p5";
 import p5Types from "p5"; //Import this for typechecking and intellisense
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
+import { useRef } from "react";
 
 // import AuthService , {LoginDto, RegisterDto} from "../../services/auth/auth.service"
 // import UserService from "../../services/auth/user.service";
+
+/**
+ * TODO:
+ * 3 game modes
+ * aspect ratio : 2 / 3
+ * height = aspect ratio * width
+ * 
+ * absolute width 1000;
+ * relative width 100%
+ * 
+ * absolute X = 720
+ * relative X = 720 / absolute width * 100
+ */ 
 
 interface GameWindowProps {
   width: number;
@@ -19,6 +33,7 @@ interface GameWindowProps {
   paddleHeight: number;
   paddleSpeed: number;
 }
+
 interface GameState {
   // Game variables
   ballX: number;
@@ -37,112 +52,132 @@ interface GameState {
 }
 
 const Pong: React.FC<GameWindowProps> = (props: GameWindowProps) => {
+
+  const [P5, setP5] = useState(0);
+  // Responsivity
+  let aspectRatio : number = 16/9;
+  let absoluteWidth : number = 1000;
+  let relativeWidth : number = props.width;
+  let absoluteHeight : number = absoluteWidth / aspectRatio;
+  let relativeHeight : number = relativeWidth / aspectRatio;
+
+  console.log(aspectRatio, absoluteWidth, absoluteHeight)
+  console.log(aspectRatio, relativeWidth, relativeHeight)
+
   //STATE
+  let mousePressed: any = useRef<boolean>(false);
+
   let ballX: number = props.initBallX;
   let ballY: number = props.initBallY;
-
   let paddleOneX: number = 0;
   let paddleOneY: number = 0;
-
   let paddleTwoX: number = props.width - props.paddleWidth;
   let paddleTwoY: number = 0;
-
-  let mousePressed: boolean = false;
-
-  let state: 0 | 1 | 2 = 0;
-  let players: Array<string> = [];
+  var state: 0|2|1 = 0;
+  let players: Array<string> = []
+  const gamestatedata: any = useRef<GameState>({
+    ballX,
+    ballY,
+    paddleOneX,
+    paddleOneY,
+    paddleTwoX,
+    paddleTwoY,
+    state,
+    players,
+  })
+  const setGameStateData = (newVal) => {
+    gamestatedata.current = {...gamestatedata.current, ...newVal}
+  }
+  const getGameStateData = () => gamestatedata.current;
 
   // draw
   const drawBall = (p5: p5Types) => {
-    p5.ellipse(ballX, ballY, props.ballRadius, props.ballRadius);
+    p5.ellipse(getGameStateData().ballX, getGameStateData().ballY, props.ballRadius, props.ballRadius);
   };
   const drawPaddleOne = (p5: p5Types) => {
-    p5.rect(paddleOneX, paddleOneY, props.paddleWidth, props.paddleHeight);
+    p5.rect(getGameStateData().paddleOneX, getGameStateData().paddleOneY, props.paddleWidth, props.paddleHeight);
   };
   const drawPaddleTwo = (p5: p5Types) => {
-    p5.rect(paddleTwoX, paddleTwoY, props.paddleWidth, props.paddleHeight);
+    p5.rect(getGameStateData().paddleTwoX, getGameStateData().paddleTwoY, props.paddleWidth, props.paddleHeight);
   };
   const handlePlayerOneInput = (p5: p5Types) => {
-    if (!mousePressed) {
+    if (!mousePressed.current) {
       //handle keys
       return;
     }
-    if (p5.mouseY > paddleOneY + props.paddleHeight / 2 + props.paddleSpeed) {
-      socket.emit("playerInput", { input: "DOWN" });
+
+    if (p5.mouseY > getGameStateData().paddleOneY + props.paddleHeight / 2 + props.paddleSpeed) {
+      socket.current.emit("playerInput", { input: "DOWN" });
     } else if (
       p5.mouseY <
-      paddleOneY + props.paddleHeight / 2 - props.paddleSpeed
+      getGameStateData().paddleOneY + props.paddleHeight / 2 - props.paddleSpeed
     ) {
-      socket.emit("playerInput", { input: "UP" });
+      socket.current.emit("playerInput", { input: "UP" });
     }
   };
   const handlePlayerTwoInput = (p5: p5Types) => {
-    if (!mousePressed) {
+    if (!mousePressed.current) {
       //handle keys
       return;
     }
-    if (p5.mouseY > paddleTwoY + props.paddleHeight / 2 + props.paddleSpeed) {
-      socket.emit("playerInput", { input: "DOWN" });
+    if (p5.mouseY > getGameStateData().paddleTwoY + props.paddleHeight / 2 + props.paddleSpeed) {
+      socket.current.emit("playerInput", { input: "DOWN" });
     } else if (
       p5.mouseY <
-      paddleTwoY + props.paddleHeight / 2 - props.paddleSpeed
+      getGameStateData().paddleTwoY + props.paddleHeight / 2 - props.paddleSpeed
     ) {
-      socket.emit("playerInput", { input: "UP" });
+      socket.current.emit("playerInput", { input: "UP" });
     }
   };
+  //resize
+
+  const onResize = ( p5: p5Types)=>{
+    console.log("resizing", p5)
+
+    absoluteWidth = 1000;
+    relativeWidth = props.width;
+  
+    absoluteHeight = absoluteWidth / aspectRatio;
+    relativeHeight = relativeWidth / aspectRatio;
+
+    if(p5) p5.resizeCanvas(relativeWidth, relativeHeight);
+  }
+
   // SETUP
   let canvas: p5Types.Renderer;
-  let socket: Socket;
+  let socket: any = useRef<Socket>(null);
   const setup = (p5: p5Types, canvasParentRef: Element) => {
-    socket = io("ws://localhost:3001");
+    console.log('p5 set')
+    setP5(p5);
+    canvas = p5.createCanvas(relativeWidth, relativeHeight).parent(canvasParentRef);
 
-    // //login
-    // const loginData : LoginDto = {email:"test@mail.com", password:"123456789"}
-    // AuthService.login(loginData)
-    // .then((d)=>{
-    //   console.log("all good",d)
-    //   console.log(AuthService.getCurrentUser());
-    //   UserService.changeUsername(Date.now().toString())
-    //   .then((e)=>{
-
-    //   })
-    // })
-    // .catch(e=>{
-    //   console.log("not so good",e)
-    // })
-    // //
-
-    console.log("w", props.width);
-    console.log("h", props.height);
-    canvas = p5.createCanvas(props.width, props.height).parent(canvasParentRef);
     canvas.mousePressed(() => {
-      mousePressed = true;
+      mousePressed.current = true;
     });
     canvas.mouseReleased(() => {
-      mousePressed = false;
-    });
-
-    socket.emit("playerJoined");
-
-    socket.on("gameState", (data: GameState) => {
-      //console.log(data);
-      ballX = data.ballX;
-      ballY = data.ballY;
-      ballX = data.ballX;
-      ballY = data.ballY;
-      paddleOneX = data.paddleOneX;
-      paddleOneY = data.paddleOneY;
-      paddleTwoX = data.paddleTwoX;
-      paddleTwoY = data.paddleTwoY;
-      state = data.state;
-      players = data.players;
+      mousePressed.current = false;
     });
   };
+  
+
+
   useEffect(() => {
-    //console.log("bruh")
+    console.log("bruh")
+    socket.current = io("ws://localhost:3001").on('connect',()=>{
+      console.log( socket.current)
+      socket.current.emit("playerJoined");
+
+      socket.current.on("gameState", (data: GameState) => {
+        //console.log(data);
+
+        setGameStateData(data);
+
+      });
+    });
+
     return () => {
       if (canvas !== undefined) canvas.remove();
-      if (socket !== undefined) socket.close();
+      if (socket.current !== undefined) socket.current.close();
     };
   }, []);
 
@@ -151,16 +186,14 @@ const Pong: React.FC<GameWindowProps> = (props: GameWindowProps) => {
     p5.background(0);
     p5.frameRate(60);
 
-    //print state
-    //console.log(state);
-
-    if (!state) {
+    //console.log(getGameStateData().state)
+    if (!getGameStateData().state) {
       p5.fill(0xffffff);
       p5.textSize(40);
       p5.text("Waiting for opponent to join...", 50, props.height / 2);
       return;
     }
-    if (state === 2) {
+    if (getGameStateData().state === 2) {
       p5.fill(0xffffff);
       p5.textSize(40);
       p5.text(
@@ -179,11 +212,12 @@ const Pong: React.FC<GameWindowProps> = (props: GameWindowProps) => {
     drawPaddleTwo(p5);
 
     //handle input
-    if (players.indexOf(socket.id) === 0) handlePlayerOneInput(p5);
-    if (players.indexOf(socket.id) === 1) handlePlayerTwoInput(p5);
+    //console.log(socket.current)
+    if (getGameStateData().players.indexOf(socket.current.id) === 0) handlePlayerOneInput(p5);
+    if (getGameStateData().players.indexOf(socket.current.id) === 1) handlePlayerTwoInput(p5);
   };
 
-  return <Sketch setup={setup} draw={draw} />;
+  return <Sketch setup={setup} draw={draw} windowResized={onResize}/>;
 };
 
 //Render using :
